fe71048f31f88b21566c53a76628dec1698dfcc4
Matthew Gardner <matthewgardner@mgardner.local>
2017-05-10 10:05:12 -0500

    Added event emitters for adding and removing an agenda item

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,12 +1,13 @@
 import {Component} from "@angular/core";
 import {Agenda} from "../agenda/agenda";
 import {Schedule} from "../schedule/schedule";
+import {ConfAgendaService} from "../../services/confAgendaService";
 
 @Component({
   selector: 'page-tabs',
   template: `
     <ion-tabs>
-      <ion-tab [root]="agenda" tabTitle="My Agenda" tabIcon="list"></ion-tab>
+      <ion-tab [root]="agenda" tabTitle="My Agenda" tabIcon="list" tabBadge="{{agendaCount}}" tabBadgeStyle="danger"></ion-tab>
       <ion-tab [root]="schedule" tabTitle="Schedule" tabIcon="calendar"></ion-tab>
     </ion-tabs>
   `
@@ -14,4 +15,17 @@ import {Schedule} from "../schedule/schedule";
 export class Tabs {
   agenda = Agenda;
   schedule =  Schedule;
+  private agendaCount: number = 0;
+
+  constructor(private agendaService: ConfAgendaService) {
+    let that = this;
+    this.agendaService.addAgendaItem.subscribe(() => {
+      that.agendaCount++;
+
+    });
+
+    this.agendaService.deleteAgendaItem.subscribe(() => {
+      that.agendaCount--;
+    });
+  }
 }
